0b0c6ba5a9665fee47e066a666d4e461ad584913
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-09-03 18:04:19 -0500

    working on search function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from "axios";
 import UserCard from './components/UserCard';
 import FollowerCard from './components/FollowerCard';
+import SearchUserForm from './components/SearchForm';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
@@ -14,6 +15,7 @@ class App extends React.Component {
     this.state = {
       User: [],
       Followers: [],
+  
     };
     console.log('Constructor is running!');
   }
@@ -21,23 +23,30 @@ class App extends React.Component {
   componentDidMount() {
     console.log('CDM is running');
     axios
-    .get("https://api.github.com/users/rojcewiczj")
+    .get(`https://api.github.com/users/rojcewiczj`)
     .then(res => this.setState({ User: res.data }))
     .catch(err => console.log("why me!?"));
     console.log(this.state.User)
  axios
-    .get("https://api.github.com/users/rojcewiczj/followers")
+    .get(`https://api.github.com/users/rojcewiczj/followers`)
     .then(res => this.setState({ Followers: res.data }))
     .catch(err => console.log("why me!?"));
     console.log(this.state.User)
   };
   
-
+  searchUser = UserName => {
+    const newUser = {
+      name: UserName,
+    };
+    this.setState({
+      userName: [newUser]
+    });
+  };
   render() {
     console.log('Rendering component');
     return (
       <div className="App">
-        <div className="user">User</div>
+        <div className="user"><SearchUserForm searchUser = {this.searchUser} /></div>
         <br></br>
       <div className="header">   
       <UserCard name={this.state.User.name} img={this.state.User.avatar_url} location={this.state.User.location} key={this.state.User.id} />
